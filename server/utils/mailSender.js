02ea6b40531b8839fee9dc25849404fc9b16f7bb
Atul Rawat <atulrawat104903@gmail.com>
2026-05-05 23:58:07 +0530

    fix: init resend inside function

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,9 +1,8 @@
 const { Resend } = require("resend");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const mailSender = async (email, title, body) => {
     try {
+        const resend = new Resend(process.env.RESEND_API_KEY);
         const info = await resend.emails.send({
             from: "Coursiqo <onboarding@resend.dev>",
             to: email,
